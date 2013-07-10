2287fddb37c6a0339dabcec6adb75c2194fa6473
yujingzhang <yujingz@andrew.cmu.edu>
2013-07-10 05:23:53 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var buffer = new Buffer(fs.readFile("index.html"));
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
